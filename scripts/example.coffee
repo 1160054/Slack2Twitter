ea6bf53657fa8cddeb46f7bd8401e9e4094056c6
小野寺優太 <developer@unknown.local>
2016-09-20 16:23:26 +0900

    init

---
+++ scripts/example.coffee
@@ -0,0 +1,11 @@
+child_process = require 'child_process'
+module.exports = (robot) ->
+  bot_name = 'post_twitter'
+  robot.hear ///#{bot_name}.*///i, (res) ->
+    shift_num = bot_name.length + 1
+    msg = res.message.text[shift_num..(140+shift_num)]
+    return if msg.match(///.*send.*///i)
+    child_process.exec "ruby ./scripts/post.rb '#{msg}'", (error, stdout, stderr) ->
+      return res.emote "stderr: \n#{stderr}" if stderr
+      return res.emote "error:  \n#{error}"  if error
+      return res.emote "output: \n#{stdout}" if stdout
---
+++ scripts/post.rb
@@ -0,0 +1,53 @@
+require 'twitter'
+class Slack2Twitter
+  def initialize(consumer_key:, consumer_secret:, access_token:, access_token_secret:, twitter_user_id:)
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = consumer_key
+      config.consumer_secret     = consumer_secret
+      config.access_token        = access_token
+      config.access_token_secret = access_token_secret
+    end
+    @twitter_user_id             = twitter_user_id
+  end
+
+  def send_tweet(args = nil)
+    return unless args
+    msg = args.join(' ')
+    return if msg[0] == '_'
+    puts "-"*50 + "\n送信\n" + "-"*50 + "\n"
+    if @client.update(msg)
+      puts "送信\n  #{msg}"
+    else
+      puts "送信失敗\n  #{msg}"
+    end
+  end
+
+  def recieve_activity(activity_name)
+    case activity_name
+    when :user_timeline
+      @client.send(activity_name, @twitter_user_id).map { |m| "#{m.text}" }[0,4]
+    when :mentions_timeline, :home_timeline
+      @client.send(activity_name).map { |m| "#{m.user.name} => #{m.text}" }[0,4]
+    end
+  rescue Twitter::Error::TooManyRequests => e
+    puts 'リクエスト多すぎて一時的にbanされてるから１５分待ってね'
+    puts e
+  end
+
+  def show_activity(activity_name)
+    puts "-"*50 + "\n#{activity_name}\n" + "-"*50 + "\n"
+    puts recieve_activity(activity_name)
+  end
+end
+
+slack2twitter = Slack2Twitter.new(consumer_key:        ENV['YOUR_CONSUMER_KEY'],
+                                  consumer_secret:     ENV['YOUR_CONSUMER_SECRET'],
+                                  access_token:        ENV['YOUR_ACCESS_TOKEN'],
+                                  access_token_secret: ENV['YOUR_ACCESS_SECRET'],
+                                  twitter_user_id:     ENV['YOUR_TWITTER_USER_ID'])
+
+[:mentions_timeline, :user_timeline].each do |activity_name|
+  slack2twitter.show_activity(activity_name)
+end
+
+slack2twitter.send_tweet(ARGV)
